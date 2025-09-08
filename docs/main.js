4f9739e5b0fe06dcde13feb2fd7bbedfd992d211
xmoraesanapaula-dotcom <xmoraesanapaula@gmail.com>
2025-09-07 22:59:32 -0300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- LÓGICA DA VERSÃO ---
     const versionInfo = document.getElementById('version-info');
     if (versionInfo) {
-        versionInfo.textContent = 'v1.5.4';
+        versionInfo.textContent = 'v1.5.3';
     }
 
     // --- v1.4: LÓGICA DO MODO ESCURO (DARK MODE) ---
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    console.log("Script principal (main.js) v1.5.4 carregado com sucesso.");
+    console.log("Script principal (main.js) v1.5.3 carregado com sucesso.");
 
     // No futuro, a lógica para animações de cards, formulários, etc., viria aqui.
 });
